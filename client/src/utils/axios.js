f065c8f93de28588de0c8f527208d4af7bc0597f
Anabil <91933794+Anabil-Baruah@users.noreply.github.com>
2023-09-05 23:16:02 +0530

    Added deployement server

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const baseURL = 'http://localhost:8000/api';
-// const baseURL = 'https://cryptomedia.onrender.com';
+// const baseURL = 'http://localhost:8000/api';
+const baseURL = 'https://zura-ventures.onrender.com';
 
 const jwtToken = localStorage.getItem('accessToken');
 axios.defaults.headers.common['Authorization'] = `${jwtToken}`;
